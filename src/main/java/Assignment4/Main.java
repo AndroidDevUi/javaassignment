dedeb718039a33d2c4f41bb502f447077b5b7983
mitcse2026 <mitcse2026@gmail.com>
2024-11-18 06:25:35 +0000

    init

---
+++ src/main/java/Assignment4/Main.java
@@ -0,0 +1,9 @@
+package Assignment4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+            "Assignment 4"
+        );
+    }
+}
--- src/main/java/com/my_project/Main.java
+++
@@ -1,7 +0,0 @@
-package com.my_project;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hii");
-    }
-}
